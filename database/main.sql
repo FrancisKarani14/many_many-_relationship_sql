7e99a5bb5c5e0decc56b77330c4b9cedbe0f0737
Francis <franciskarani15@gmail.com>
2025-08-21 15:02:51 +0300

    creates tables doctors, patients and the doctors_patients join table

--- database/main.sql
+++ database/main.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS doctors(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 first_name TEXT NOT NULL,
-second_name TEXT NOT NULL,
+second_name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS patients(
-id INTERGER PRIMARY KEY AUTOINCREMENT,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 first_name TEXT NOT NULL,
 second_name TEXT NOT NULL,
 dicease TEXT NOT NULL
-
-
 );
 
 CREATE TABLE IF NOT EXISTS doctors_patients(
 doctor_id INTEGER,
 patient_id INTEGER,
-FOREIGN
+PRIMARY KEY (doctor_id, patient_id),
+FOREIGN KEY (doctor_id) REFERENCES doctors(id),
+FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
