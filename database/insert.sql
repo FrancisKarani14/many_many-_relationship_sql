4e5fc55f8c1f106a670de0becea52a160d24653c
Francis <franciskarani15@gmail.com>
2025-08-21 15:52:05 +0300

    alters the doctors table

---
+++ database/insert.sql
@@ -0,0 +1,1 @@
+INSERT OR IGNORE INTO doctors ("first_name", "second_name", "specialization")
--- database/main.sql
+++ database/main.sql
@@ -1,8 +1,12 @@
+-- creates a doctors table
+-- a doctro can have many patients and a patient can have many doctors
 CREATE TABLE IF NOT EXISTS doctors(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 first_name TEXT NOT NULL,
 second_name TEXT NOT NULL
 );
+ALTER TABLE doctors ADD COLUMN specialization TEXT NOT NULL; 
+-- creates a patients table
 
 CREATE TABLE IF NOT EXISTS patients(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,7 +14,9 @@ first_name TEXT NOT NULL,
 second_name TEXT NOT NULL,
 dicease TEXT NOT NULL
 );
-
+-- create the joint table, a joint between the doctors and patients table
+-- it has a collumn fo the primary key that has a the patients id and the doctors is
+-- it also has foreign keys that reference to specific tables 
 CREATE TABLE IF NOT EXISTS doctors_patients(
 doctor_id INTEGER,
 patient_id INTEGER,
